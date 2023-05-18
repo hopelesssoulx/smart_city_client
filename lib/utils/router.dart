cb323edc1ad7284380ada7443d0e42dc03b8ec95
hopelesssoulx <hopelesssoulx@gmail.com>
2023-05-18 10:20:28 +0800

    feat: router interceptor

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:smart_city_client/pages/home/news_detail.dart';
 import 'package:smart_city_client/pages/me/login.dart';
 import 'package:smart_city_client/pages/me/register.dart';
 import 'package:smart_city_client/pages/me/update_pwd.dart';
 import 'package:smart_city_client/pages/me/userinfo.dart';
+import 'package:smart_city_client/utils/token.dart';
 
 class RouteManager {
   // 路由总表
@@ -17,6 +19,16 @@ class RouteManager {
   // 自定义路由
   MaterialPageRoute routeWithSetting(RouteSettings setting) {
     WidgetBuilder? builder = _routeMap[setting.name];
+    if (authPath.contains(setting.name)) {
+      if (Token.getToken() == null) {
+        Fluttertoast.showToast(msg: '未登录', gravity: ToastGravity.CENTER);
+        return MaterialPageRoute(
+            builder: (context) {
+              return Login();
+            },
+            settings: setting);
+      }
+    }
     if (builder != null) {
       return MaterialPageRoute(builder: builder, settings: setting);
     }
@@ -27,6 +39,9 @@ class RouteManager {
   MaterialPageRoute unknowRouteWithSetting(RouteSettings setting) {
     return MaterialPageRoute(builder: (context) => Scaffold());
   }
+
+  // 需要登录
+  final authPath = ["/me/userInfo", "/me/updatePwd"];
 
   // 我的
   Map<String, WidgetBuilder> home() {
